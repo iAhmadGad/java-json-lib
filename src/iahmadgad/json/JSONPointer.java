715dc36617d7260249491ff4904f87a67d7c4c1b
iAhmadGad <ahmadasaadgad@gmail.com>
2023-09-01 13:47:22 +0300

    Create JSONPointer

---
+++ src/iahmadgad/json/JSONPointer.java
@@ -0,0 +1,24 @@
+package iahmadgad.json;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class JSONPointer 
+{
+	private static Object pointee;
+	
+	public JSONPointer(ArrayList<Object> array, String path)
+	{
+		
+	}
+	
+	public JSONPointer(HashMap<String, Object> object, String path)
+	{
+		
+	}
+	
+	public Object getPointee()
+	{
+		return pointee;
+	}
+}
