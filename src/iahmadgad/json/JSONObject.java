7f288966ff157482ae55360cba10aff25e6d1efa
iAhmadGad <ahmadasaadgad@gmail.com>
2023-09-01 18:14:36 +0300

    Revert "Update JSONArray"
    This reverts commit 4c7189dc754f7e4ec72912f66d664a2f478cdc87.

--- src/iahmadgad/json/JSONObject.java
+++ src/iahmadgad/json/JSONObject.java
@@ -1,5 +1,6 @@
 package iahmadgad.json;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class JSONObject 
@@ -51,79 +52,68 @@ public class JSONObject
 		return (double) Object.get(key);
 	}
 	
-	public JSONObject getJSONObject(String key)
+	public HashMap<String, Object> getJSONObject(String key)
 	{
-		return (JSONObject) Object.get(key);
+		return (HashMap<String, Object>) Object.get(key);
 	}
 	
-	public JSONArray getJSONArray(String key)
+	public ArrayList<Object> getJSONArray(String key)
 	{
-		return (JSONArray) Object.get(key);
+		return (ArrayList<Object>) Object.get(key);
 	}
 	
 	public void add(JSONPointer pointer, Object object)
 	{
-		pointer.setObject(this);
 		if(Validator.isValid(object)) getJSONArray(pointer).add(object);
 	}
 	
 	public void set(JSONPointer pointer, int index, Object object)
 	{
-		pointer.setObject(this);
 		if(Validator.isValid(object)) getJSONArray(pointer).set(index, object);
 	}
 	
 	public void put(JSONPointer pointer, String key, Object value)
 	{
-		pointer.setObject(this);
 		if(Validator.isValid(value)) getJSONObject(pointer).put(key, value);
 	}
 	
 	public void replace(JSONPointer pointer, String key, Object value)
 	{
-		pointer.setObject(this);
 		if(Validator.isValid(value)) getJSONObject(pointer).replace(key, value);
 	}
 	
 	public Object get(JSONPointer pointer)
 	{
-		pointer.setObject(this);
 		return pointer.getPointee();
 	}
 	
 	public String getString(JSONPointer pointer)
 	{
-		pointer.setObject(this);
 		return (String) pointer.getPointee();
 	}
 	
 	public boolean getBoolean(JSONPointer pointer)
 	{
-		pointer.setObject(this);
 		return (boolean) pointer.getPointee();
 	}
 	
 	public int getInteger(JSONPointer pointer)
 	{
-		pointer.setObject(this);
 		return (int) pointer.getPointee();
 	}
 	
 	public double getDouble(JSONPointer pointer)
 	{
-		pointer.setObject(this);
 		return (double) pointer.getPointee();
 	}
 	
-	public JSONObject getJSONObject(JSONPointer pointer)
+	public HashMap<String, Object> getJSONObject(JSONPointer pointer)
 	{
-		pointer.setObject(this);
-		return (JSONObject) pointer.getPointee();
+		return (HashMap<String, Object>) pointer.getPointee();
 	}
 	
-	public JSONArray getJSONArray(JSONPointer pointer)
+	public ArrayList<Object> getJSONArray(JSONPointer pointer)
 	{
-		pointer.setObject(this);
-		return (JSONArray) pointer.getPointee();
+		return (ArrayList<Object>) pointer.getPointee();
 	}
 }
