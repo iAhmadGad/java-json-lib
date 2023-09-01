0e6927629833e3dc48a96ef9cd608c4de64dc29b
iAhmadGad <ahmadasaadgad@gmail.com>
2023-09-01 20:55:11 +0300

    Create JSONStringBuilder Class

---
+++ src/iahmadgad/json/JSONStringBuilder.java
@@ -0,0 +1,21 @@
+package iahmadgad.json;
+
+public class JSONStringBuilder 
+{
+	public JSONStringBuilder(JSONObject object)
+	{
+		
+	}
+	
+	public JSONStringBuilder(JSONArray array)
+	{
+		
+	}
+	
+	private static String JSONString;
+	
+	public String getJSONString()
+	{
+		return JSONString;
+	}
+}
