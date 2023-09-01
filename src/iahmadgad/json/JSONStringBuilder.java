5efd259b9453804927ebc6a39aebc9adff43c13c
iAhmadGad <ahmadasaadgad@gmail.com>
2023-09-01 21:46:40 +0300

    Create Settings Subclass in JSONStringBuilder Class

--- src/iahmadgad/json/JSONStringBuilder.java
+++ src/iahmadgad/json/JSONStringBuilder.java
@@ -18,4 +18,26 @@ public class JSONStringBuilder
 	{
 		return JSONString;
 	}
+	
+	public class Settings
+	{
+		private int indentation = 3, spaceAroundColon = 0;
+		private Object[] defaults = {3, 0};
+		
+		public void setIndentation(int i)
+		{
+			indentation = i;
+		}
+		
+		public void setSpaceAroundColon(int i)
+		{
+			indentation = i;
+		}
+		
+		public void setDefault()
+		{
+			indentation = 3;
+			spaceAroundColon = 0;
+		}
+	}
 }
